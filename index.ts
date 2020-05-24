a2cb9f7da8ed4054f156387d0f02d25bc0500d12
Eduardo Campver <eduardo.campver@almundo.com>
2020-05-24 10:52:48 -0300

    chore: reorganize code

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as path from 'path';
-import { yamlToDef } from './utils';
-import { generateCode } from './codegen';
-import { ApiDef } from './types';
 import * as yargs from 'yargs';
+import { yamlToDef } from './src/utils';
+import { generateCode } from './src/codegen';
+import { ApiDef } from './src/types';
 
 
 const { source, destination, clientName } = yargs
